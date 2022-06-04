dbe9ff4d7f3a4c687635ab8b690eecd16ebcee36
Amarillia31 <mari31@inbox.ru>
2022-06-05 01:30:27 +0400

    video added

--- src/test/java/com/elena/TestBase.java
+++ src/test/java/com/elena/TestBase.java
@@ -17,6 +17,12 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        //video configuration
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
@@ -24,7 +30,7 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-//        Attach.addVideo();
+        Attach.addVideo();
 //        closeWebDriver();
     }
 }
