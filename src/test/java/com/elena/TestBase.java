4b2f4ed7767bdc1187326c9e1cf2ed2208c59fb4
Amarillia31 <mari31@inbox.ru>
2022-06-12 15:57:48 +0400

    owner lib added

--- src/test/java/com/elena/TestBase.java
+++ src/test/java/com/elena/TestBase.java
@@ -1,22 +1,32 @@
 package com.elena;
 
+import Config.CredentialsConfig;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import helpers.Attach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
     static void setUp() {
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        String login = config.login();
+        String password = config.password();
+        String remoteDriver = System.getProperty("remoteDriver", "selenoid.autotests.cloud/wd/hub");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+        String baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+
+
+//        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = baseUrl;
+        Configuration.browserSize = browserSize;
+        Configuration.remote = "https://" + login + ":" + password + "@" + remoteDriver;
 
         //video configuration
         DesiredCapabilities capabilities = new DesiredCapabilities();
